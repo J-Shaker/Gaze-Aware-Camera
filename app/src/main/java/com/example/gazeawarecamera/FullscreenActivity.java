b50353d996842f0021b8aef2e674cc5ee221a8d7
John <jshaker822@gmail.com>
2021-10-19 18:51:50 -0400

    File commit

---
+++ app/src/main/java/com/example/gazeawarecamera/FullscreenActivity.java
@@ -0,0 +1,338 @@
+package com.example.gazeawarecamera;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.Size;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowInsets;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.example.gazeawarecamera.databinding.ActivityFullscreenBinding;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends AppCompatActivity {
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+     */
+    private static final boolean AUTO_HIDE = true;
+
+    /**
+     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+     * user interaction before hiding the system UI.
+     */
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * Some older devices needs a small delay between UI widget updates
+     * and a change of the status and navigation bar.
+     */
+    private static final int UI_ANIMATION_DELAY = 300;
+    private final Handler mHideHandler = new Handler();
+    private View mContentView;
+    private final Runnable mHidePart2Runnable = new Runnable() {
+        @SuppressLint("InlinedApi")
+        @Override
+        public void run() {
+            // Delayed removal of status and navigation bar
+            if (Build.VERSION.SDK_INT >= 30) {
+                mContentView.getWindowInsetsController().hide(
+                        WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+            } else {
+                // Note that some of these constants are new as of API 16 (Jelly Bean)
+                // and API 19 (KitKat). It is safe to use them, as they are inlined
+                // at compile-time and do nothing on earlier devices.
+                mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            }
+        }
+    };
+    private View mControlsView;
+    private final Runnable mShowPart2Runnable = new Runnable() {
+        @Override
+        public void run() {
+            // Delayed display of UI elements
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.show();
+            }
+            mControlsView.setVisibility(View.VISIBLE);
+        }
+    };
+    private boolean mVisible;
+    private final Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            hide();
+        }
+    };
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            switch (motionEvent.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    if (AUTO_HIDE) {
+                        delayedHide(AUTO_HIDE_DELAY_MILLIS);
+                    }
+                    break;
+                case MotionEvent.ACTION_UP:
+                    view.performClick();
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        }
+    };
+    private ActivityFullscreenBinding binding;
+
+    /*
+     * Camera set up
+     */
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    PreviewView previewView;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityFullscreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mVisible = true;
+
+        mControlsView = binding.fullscreenContentControls;
+        mContentView = binding.fullscreenContent;
+
+        // Set up the user interaction to manually show or hide the system UI.
+        mContentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { toggle(); }
+        });
+
+        // Upon interacting with UI controls, delay any scheduled hide()
+        // operations to prevent the jarring behavior of controls going away
+        // while interacting with the UI.
+        binding.button2.setOnTouchListener(mDelayHideTouchListener);
+
+        /*
+         * Camera set up.
+         */
+        getPermissionToUseCamera();
+        previewView = findViewById(R.id.fullscreenContent);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindUseCases(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100);
+    }
+
+    private void toggle() {
+        if (mVisible) {
+            hide();
+        } else {
+            show();
+        }
+    }
+
+    private void hide() {
+        // Hide UI first
+        /*ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        mControlsView.setVisibility(View.GONE);
+        mVisible = false;*/
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.removeCallbacks(mShowPart2Runnable);
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    private void show() {
+        // Show the system bar
+        if (Build.VERSION.SDK_INT >= 30) {
+            mContentView.getWindowInsetsController().show(
+                    WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+        } else {
+            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        }
+        mVisible = true;
+
+        // Schedule a runnable to display UI elements after a delay
+        /*mHideHandler.removeCallbacks(mHidePart2Runnable);
+        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);*/
+    }
+
+    // Called when a user clicks the 'album' button
+    /*public void openAlbum (View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(
+                "content://media/internal/images/media"));
+        startActivity(intent);
+    }*/
+    public void openAlbum(View view) {
+        Button albumButton = (Button)findViewById(R.id.button1);
+        albumButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void menu (View view) {
+        Button menuButton = (Button) findViewById(R.id.button3);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu menu = new PopupMenu(FullscreenActivity.this, menuButton);
+                menu.getMenuInflater().inflate(R.menu.popup_menu, menu.getMenu());
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick (MenuItem item) {
+                        Toast.makeText(FullscreenActivity.this,
+                                "Item Clicked: " + item.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                menu.show();
+            }
+        });
+    }
+
+    /**
+     * Schedules a call to hide() in delay milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    private void getPermissionToUseCamera() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 100);
+        }
+    }
+
+    /*
+     * The majority of the code in the bindUseCases method comes from official Android Documentation
+     * found at https://developer.android.com/training/camerax. The code is responsible only for the
+     * set up of camera usage in an Android application using the CameraX API. We have provided
+     * comments below which explain the function of each code snippet.
+     */
+    private void bindUseCases(@NonNull ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+        /*
+         * First we need a CameraSelector object that tells our application which camera to use. For
+         * now, we are defaulting to using the front camera, as that is most appropriate for our
+         * use case.
+         */
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+                .build();
+        /*
+         * Next we create an ImageAnalysis object which allows us to analyze each frame the camera
+         * produces in real time. We are currently capturing frames at 720p, though we may wish to
+         * choose a lower resolution in the future to enhance processing speed. We also set the
+         * ImageAnalysis object to analyze only the latest frame as we only care about what is
+         * happening at the singular, latest instance.
+         */
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setTargetResolution(new Size(1280, 720))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+        /*
+         * We are implementing a custom analyzer for our ImageAnalysis object. Specifically, our
+         * gaze detection algorithm. Here we override the default analyze method of ImageAnalysis.
+         */
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new ImageAnalysis.Analyzer() {
+            @Override
+            public void analyze(@NonNull ImageProxy image) {
+                // We implement our analysis code here
+                image.close();
+            }
+        });
+        /*
+         * Then we create an ImageCapture object so that we may save individual frames as photos. We
+         * need to create a method that handles saving an image, and this method will be called both
+         * by the onClick method and the analyze method which is implemented above.
+         */
+        ImageCapture imageCapture = new ImageCapture.Builder()
+                .setTargetRotation(previewView.getDisplay().getRotation())
+                .build();
+        /*
+         * Finally we create a preview object which provides the camera feed to our layout (GUI). In
+         * order for the GUI to function properly, we need to set the surface provider. We are
+         * letting Android set the surface provider and using that.
+         */
+        Preview preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        /*
+         * Tying it all together is the bindToLifecycle method of cameraProvider which executes the
+         * three use cases established above.
+         */
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, imageCapture, imageAnalysis, preview);
+    }
+}
